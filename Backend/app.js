2a73070b0e592e5f74999cd4feb76beb0fcc33a4
CKsisodia <cksisodia3@gmail.com>
2024-07-04 16:07:52 +0530

    integrate helmet and morgon

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
+const morgon = require("morgan");
 const cors = require("cors");
+const fs = require("fs");
+const path = require('path')
 
 const app = express();
+app.use(helmet());
 
 const sequelize = require("./db/database");
 const authRoutes = require("./routes/auth");
@@ -11,13 +17,21 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors());
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+
+app.use(morgon("combined", { stream: accessLogStream }));
+
 app.use("/user", authRoutes);
 app.use("/expense", expenseRoutes);
 
 sequelize
   .sync()
   .then(
-    app.listen(5000, () => {
+    app.listen(process.env.APP_RUNNING_PORT, () => {
       console.log("Server is running on port 5000");
     })
   )
